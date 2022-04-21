fad6d688c4a85136d396d8bbe3287b3d626a4c07
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2022-04-21 12:03:57 -0300

    Adjust background

--- main/main.js
+++ main/main.js
@@ -1,180 +1,185 @@
-window.onload = function (){
+window.onload = function () {
   test();
   setColor();
   bounce();
 };
 
 /*============[BOUNCE ANIMATION]=============*/
-var time=500;
-function bounce(){
-  var id ="#";
-  id += Math.floor(Math.random()*5+1);
-  
-  var isUp = ((id=="#"+selected) ||
-  (sqPlay.indexOf(id)>=0));
-  gsq.forEach(function (value){
-    if(id == ("#" + value)){
+var time = 500;
+function bounce() {
+  var id = "#";
+  id += Math.floor(Math.random() * 5 + 1);
+
+  var isUp = id == "#" + selected || sqPlay.indexOf(id) >= 0;
+  gsq.forEach(function (value) {
+    if (id == "#" + value) {
       isUp = true;
     }
   });
-  
-  if((id == "#"+ssq) || isUp){return bounce();}
 
-  $(id).playKeyframe({name:"bounce",
-        duration: "700ms",
-        timing: "linear"
-  });
-  time = Math.floor(Math.random()*5000+1000);
+  if (id == "#" + ssq || isUp) {
+    return bounce();
+  }
+
+  $(id).playKeyframe({ name: "bounce", duration: "700ms", timing: "linear" });
+  time = Math.floor(Math.random() * 5000 + 1000);
   setTimeout(bounce, time);
 }
 
-var opened=false;
-var running=false;
+var opened = false;
+var running = false;
 var changeStateOn = false;
-var toggle=[false, true, true];
+var toggle = [false, true, true];
 var selecting;
 var selected;
-var inState=true;
+var inState = true;
 var sel = $(".selection1");
 
-function select(num, replace = false){
-  if(inState 
-  //|| (gsq.get(num)>=0 && !replace)
-  ){return;}
+function select(num, replace = false) {
+  if (inState) {
+    return;
+  }
   ani(selected, true, num);
   selected = null;
   selecting = false;
 }
-function toGradient(){
-  if(inState && !changeStateOn){
+function toGradient() {
+  if (inState && !changeStateOn) {
     changeStateOn = true;
     clear();
     $(".sButton").css({
-      background: "rgba(255, 255, 255, 0.0)"
+      background: "rgba(255, 255, 255, 0.0)",
     });
     $(".gButton").css({
-      background: "rgba(255, 255, 255, 0.5)"
+      background: "rgba(255, 255, 255, 0.5)",
     });
-    $(".view").playKeyframe(
-    "translate 2s reverse");
-    setTimeout(function (){
+    $(".view").playKeyframe("translate 2s reverse");
+    setTimeout(function () {
       $(".view").css({
         borderRadius: "100%",
         width: "0px",
-        height: "0px"
+        height: "0px",
       });
       open();
     }, 1950);
   }
 }
-function toSolid(){
-  if(!inState && !changeStateOn){
+function toSolid() {
+  if (!inState && !changeStateOn) {
     changeStateOn = true;
     clear();
     $(".gButton").css({
-      background: "rgba(255, 255, 255, 0.0)"
+      background: "rgba(255, 255, 255, 0.0)",
     });
     $(".sButton").css({
-      background: "rgba(255, 255, 255, 0.5)"
+      background: "rgba(255, 255, 255, 0.5)",
     });
     open();
-    setTimeout(function (){
-      $(".view").playKeyframe("translate 2s",
-      function (){
+    setTimeout(function () {
+      $(".view").playKeyframe("translate 2s", function () {
         $(".view").css({
           borderRadius: "0%",
           width: "300px",
-          height: "300px"
+          height: "300px",
         });
         inState = true;
         changeStateOn = false;
       });
-      
     }, 5000);
   }
 }
 
 /*=========[GRADIENT STATE TRANSITION]========*/
-function open(){
-  if(running){return;}
-  if(opened){
-    
+function open() {
+  if (running) {
+    return;
+  }
+  if (opened) {
     opened = false;
     running = true;
-    
-    fill1.playKeyframe({name: "slide3", 
-    duration:"2s", direction:"reverse",
-    timingFunction: "ease-in-out"});
 
-    fill.playKeyframe({name: "slide2", 
-    duration:"2s", direction:"reverse",
-    timingFunction: "ease-in-out"});
-    setTimeout( function (){
+    fill1.playKeyframe({
+      name: "slide3",
+      duration: "2s",
+      direction: "reverse",
+      timingFunction: "ease-in-out",
+    });
+
+    fill.playKeyframe({
+      name: "slide2",
+      duration: "2s",
+      direction: "reverse",
+      timingFunction: "ease-in-out",
+    });
+    setTimeout(function () {
       fill.css({
         right: "125px",
         visibility: "hidden",
         width: "0px",
-        height: "0px"
+        height: "0px",
       });
       fill1.css({
         left: "125px",
         visibility: "hidden",
         width: "0px",
-        height: "0px"
+        height: "0px",
       });
-      sel.playKeyframe({name: "translate2",
-      duration: "2s", direction: "reverse",
-      timingFunction: "ease-in-out"});
-      setTimeout(function (){
+      sel.playKeyframe({
+        name: "translate2",
+        duration: "2s",
+        direction: "reverse",
+        timingFunction: "ease-in-out",
+      });
+      setTimeout(function () {
         sel.css({
           width: "0px",
-          visibility: "hidden"
+          visibility: "hidden",
         });
         $(".view1").playKeyframe("shrink 1s");
-        setTimeout(function (){
-          $(".view1").css({height: "0px"});
+        setTimeout(function () {
+          $(".view1").css({ height: "0px" });
           running = false;
         }, 1000);
       }, 2000);
     }, 2000);
-    
-  }else{
+  } else {
     running = true;
-    
-    animate($(".view1"), shrink," 1s reverse");
-    setTimeout(function (){
-      $(".view1").css({height: "300px"});
-      
-      opened=true;
-    
+
+    animate($(".view1"), shrink, " 1s reverse");
+    setTimeout(function () {
+      $(".view1").css({ height: "300px" });
+
+      opened = true;
+
       sel.css({
-        visibility: "visible"
+        visibility: "visible",
       });
       animate(sel, stretch, {
-      duration:"2s"});
-      setTimeout(function (){
+        duration: "2s",
+      });
+      setTimeout(function () {
         sel.css({
           width: "600px",
-          visibility: "visible"
+          visibility: "visible",
         });
-        
-        fill1.playKeyframe({name:"slide3",
-        duration: "2s" });
+
+        fill1.playKeyframe({ name: "slide3", duration: "2s" });
 
         animate(fill, expand, {
-        duration: "2s"});
-        setTimeout(function (){
+          duration: "2s",
+        });
+        setTimeout(function () {
           fill.css({
             right: "0px",
             visibility: "visible",
             width: "250px",
-            height: "250px"
+            height: "250px",
           });
           fill1.css({
             left: "0px",
             visibility: "visible",
             width: "250px",
-            height: "250px"
+            height: "250px",
           });
           running = false;
           inState = false;
@@ -186,24 +191,24 @@ function open(){
 }
 var sqSize = 14.57726;
 
-function test(){
+function test() {
   $(".sButton").css({
-      background: "rgba(255, 255, 255, 0.5)"
+    background: "rgba(255, 255, 255, 0.5)",
   });
   backColor(getRandomColor());
   borderColor(getRandomColor());
-  var body = document.getElementsByClassName
-   ("square1");
-  space = (100 - (14.57726*body.length))/
-    (body.length*2);
-  var pos = document.getElementsByClassName
-  ("box3")[0].getBoundingClientRect();
+  var body = document.getElementsByClassName("square1");
+  space = (100 - 14.57726 * body.length) / (body.length * 2);
+  var pos = document.getElementsByClassName("box3")[0].getBoundingClientRect();
   console.log(pos.right);
 }
 
 /*========[SQUARE ANIMATION VARIABLES]========*/
 var ssq = null;
-var gsq = new Map([[0, -1], [1, -1]]);
+var gsq = new Map([
+  [0, -1],
+  [1, -1],
+]);
 var space;
 var sqPlay = [];
 var toClear = true;
@@ -211,45 +216,39 @@ var gradColor = {
   start: "linear-gradient(to bottom right,",
   color1: "",
   color2: "",
-  end: ")"
+  end: ")",
 };
-  
+
 /*===========[SQUARE IN ANIMATION]============*/
-function ani(id, chooseOrClear=false, num = -1){
-  if(changeStateOn && !chooseOrClear){
+function ani(id, chooseOrClear = false, num = -1) {
+  if (changeStateOn && !chooseOrClear) {
     return;
   }
-  var sq = $("#"+id);
-  var sq1 = $("#"+ssq);
+  var sq = $("#" + id);
+  var sq1 = $("#" + ssq);
 
-  if(!inState){
+  if (!inState) {
     var selectable = true;
     sq1 = null;
-    gsq.forEach(function(value, key){
-      if(id == value){
+    gsq.forEach(function (value, key) {
+      if (id == value) {
         num = key;
         sq1 = sq;
         selectable = false;
       }
     });
-    if(selected && selected != id && 
-    num>-1){
+    if (selected && selected != id && num > -1) {
       select(num, true);
-      //alert("im");
       return;
     }
-    if(selectable){
-      sq1 = $("#"+gsq.get(num));
+    if (selectable) {
+      sq1 = $("#" + gsq.get(num));
     }
-    if(!chooseOrClear && selectable){
-      if(selected){
-        $("#"+selected).playKeyframe(
-          "rSelect 0.5s forwards"
-        );
+    if (!chooseOrClear && selectable) {
+      if (selected) {
+        $("#" + selected).playKeyframe("rSelect 0.5s forwards");
       }
-      sq.playKeyframe(
-        "bubble 0.7s linear forwards"
-      );
+      sq.playKeyframe("bubble 0.7s linear forwards");
       selected = id;
       return;
     }
@@ -257,152 +256,133 @@ function ani(id, chooseOrClear=false, num = -1){
 
   var index = sq.index();
   var index1 = 0;
-  if(sq1){index1 = sq1.index();}
-  /*
-    190 initial left
-    - 147 padding
-    = 43 
-    43 + 25
-    68/686
-    218
-    53.6443
-    368
-  */
-  var pos1, pos2;
-  if(!inState){
-    var inNum = 100*(218+(150*num))/686;
-    pos1 = (((inNum)-space)-(sqSize*index)
-    -(space*2*index))+"%";
-    inNum = 100*(218+(150*num))/686;
-    pos2 = (((inNum)-space)-(sqSize*index1)
-    -(space*2*index1))+"%";
-  }else{
-    backColor(sq[0].style.backgroundColor);
-    pos1 = (42.71137-space
-    -(sqSize*index)-(space*2*index))+"%";
-    pos2 = (42.71137-space
-    -(sqSize*index1)-(space*2*index1))+"%";
+  if (sq1) {
+    index1 = sq1.index();
   }
-/*-----------[SQUARE ANIMATION BACK]-----------*/
-  if(sq1[0] && (sqPlay.indexOf("#"+sq1[0].id)<0)){
-  
+  var pos1, pos2;
+  if (!inState) {
+    var inNum = (100 * (218 + 150 * num)) / 686;
+    pos1 = inNum - space - sqSize * index - space * 2 * index + "%";
+    inNum = (100 * (218 + 150 * num)) / 686;
+    pos2 = inNum - space - sqSize * index1 - space * 2 * index1 + "%";
+  } else {
+    backColor(sq[0].style.backgroundColor);
+    pos1 = 42.71137 - space - sqSize * index - space * 2 * index + "%";
+    pos2 = 42.71137 - space - sqSize * index1 - space * 2 * index1 + "%";
+  }
+  /*-----------[SQUARE ANIMATION BACK]-----------*/
+  if (sq1[0] && sqPlay.indexOf("#" + sq1[0].id) < 0) {
     toClear = true;
-    sqPlay.push("#"+sq1[0].id);
+    sqPlay.push("#" + sq1[0].id);
     animate(sq1, sqBack, " 0.5s", {
-      "0%": {"left": pos2}
+      "0%": { left: pos2 },
     });
-    setTimeout(function (){
-      
-      if(toClear){
+    setTimeout(function () {
+      if (toClear) {
         ssq = null;
-      
-        if(gsq.has(num)&&gsq.get(num)==sq1[0].id){
+
+        if (gsq.has(num) && gsq.get(num) == sq1[0].id) {
           gsq.set(num, -1);
         }
       }
-      //alert("in "+ssq+" "+sq1[0].id);
       sq1.css({
         left: 0,
         top: 0,
-        transform: ""
+        transform: "",
       });
-      //setTimeout(function (){
-        sqPlay.splice(
-        sqPlay.indexOf("#"+sq1[0].id), 1);
-     // }, 200);
+      sqPlay.splice(sqPlay.indexOf("#" + sq1[0].id), 1);
     }, 400);
   }
-  
-/*---------[SQUARE ANIMATION FORWARD]---------*/
-  if(sq[0] !== sq1[0]){
+
+  /*---------[SQUARE ANIMATION FORWARD]---------*/
+  if (sq[0] !== sq1[0]) {
     toClear = false;
-    
-    if(!inState){
-      gradColor["color"+(num+1)]= 
-      ((num>0)?", ":" ")+sq[0].
-      style.backgroundColor;
-      
-      backColor({ backgroundImage:
-        gradColor.start +
-        gradColor.color1 +
-        gradColor.color2 +
-        gradColor.end
+
+    document.querySelector("body").style.backgroundSize = `100% ${
+      document.querySelector("body").scrollHeight
+    }px`;
+    if (!inState) {
+      gradColor["color" + (num + 1)] =
+        (num > 0 ? ", " : " ") + sq[0].style.backgroundColor;
+
+      backColor({
+        backgroundImage:
+          gradColor.start + gradColor.color1 + gradColor.color2 + gradColor.end,
       });
-      
+
       gsq.set(num, id);
-    }else{
+    } else {
       ssq = sq[0].id;
     }
-    animate(sq, sqForward, 
-    " 0.5s", {
-      "100%": {"left": pos1}
-    },
-    function (){
-      sq.css({
-        left: pos1,
-        top: 225+"px",
-        transform: "rotate(90deg) "+
-        "scale(1.5,1.5)"
-      });
-      console.log("sjs");
-    }, 450
+    animate(
+      sq,
+      sqForward,
+      " 0.5s",
+      {
+        "100%": { left: pos1 },
+      },
+      function () {
+        sq.css({
+          left: pos1,
+          top: 225 + "px",
+          transform: "rotate(90deg) " + "scale(1.5,1.5)",
+        });
+        console.log("sjs");
+      },
+      450
     );
   }
 }
 
 /*================[CLEAR]==================*/
-function clear(){
-  gsq.forEach(function (x, key){
-    if(x >= 0){
+function clear() {
+  gsq.forEach(function (x, key) {
+    if (x >= 0) {
       ani(x, true);
     }
-    setTimeout(function (){
+    setTimeout(function () {
       gsq.set(key, -1);
     }, 500);
-    
   });
-  if(ssq){
+  if (ssq) {
     ani(ssq, true);
-    setTimeout(function (){
+    setTimeout(function () {
       ssq = null;
     }, 500);
   }
-  
+
   selected = null;
 }
 
-function borderColor(color){
-  var borders = document.
-  getElementsByClassName("border");
-  for(var i=0; i<borders.length; i++){
+function borderColor(color) {
+  var borders = document.getElementsByClassName("border");
+  for (var i = 0; i < borders.length; i++) {
     borders[i].style.borderColor = color;
   }
-  document.getElementsByClassName("b")[0]
-  .style.backgroundColor
-  = color;
+  document.getElementsByClassName("b")[0].style.backgroundColor = color;
 }
 
-function backColor(back){
-  if(typeof(back)=="object"){
+function backColor(back) {
+  if (typeof back == "object") {
     $(".scaff").css(back);
     return;
   }
   $(".scaff").css({
     backgroundImage: "",
-    background: back
+    background: back,
   });
 }
 
-function setColor(){
+function setColor() {
   var v = $(".square1");
-  for(var i=0; i<v.length;i++){
+  for (var i = 0; i < v.length; i++) {
     v[i].style.backgroundColor = getRandomColor();
   }
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
+  var letters = "0123456789ABCDEF";
+  var color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.round(Math.random() * 15)];
   }
@@ -417,83 +397,76 @@ var sqBack = {
   "0%": {
     top: "225px",
     left: "293px",
-    transform: "rotate(90deg) scale(1.5,1.5)"
+    transform: "rotate(90deg) scale(1.5,1.5)",
   },
   "100%": {
     top: "0px",
     left: "0px",
-    transform: "rotate(0deg) scale(1, 1)"
-  }
-}
+    transform: "rotate(0deg) scale(1, 1)",
+  },
+};
 var sqForward = {
   name: "sqForward",
   "0%": {
     top: "0px",
     left: "0px",
-    transform: "rotate(0deg) scale(1, 1)"
+    transform: "rotate(0deg) scale(1, 1)",
   },
   "100%": {
     top: "225px",
     left: "293px",
-    transform: "rotate(90deg) scale(1.5,1.5)"
-  }
-}
+    transform: "rotate(90deg) scale(1.5,1.5)",
+  },
+};
 
 var frameNames = {};
-function animate(obj, frame, play,
-valuesOrCall={}, callOrTime, time){
+function animate(obj, frame, play, valuesOrCall = {}, callOrTime, time) {
   var temp = frame;
-  if(typeof(valuesOrCall)=="object"){
-    for (const [key1, value1] of 
-    Object.entries(valuesOrCall)){
-      for (const [key2, value2] of 
-      Object.entries(value1)){
+  if (typeof valuesOrCall == "object") {
+    for (const [key1, value1] of Object.entries(valuesOrCall)) {
+      for (const [key2, value2] of Object.entries(value1)) {
         temp[key1][key2] = value2;
       }
     }
   }
   var anNum = 0;
-  if(temp.name in frameNames){
+  if (temp.name in frameNames) {
     anNum = 1;
-    while(frameNames[temp.name].
-    indexOf(anNum)>=0){
+    while (frameNames[temp.name].indexOf(anNum) >= 0) {
       anNum++;
     }
     frameNames[temp.name].push(anNum);
-  }else{
-    frameNames[temp.name] = [0]
+  } else {
+    frameNames[temp.name] = [0];
   }
-  temp.name = temp.name+anNum;
-  
-  $.keyframe.define([
-    temp
-  ]);
-  if(typeof(callOrTime)=="number"){
+  temp.name = temp.name + anNum;
+
+  $.keyframe.define([temp]);
+  if (typeof callOrTime == "number") {
     time = callOrTime;
   }
-  if(typeof(play)=="object"){
+  if (typeof play == "object") {
     play["name"] = temp.name;
-    if(!time){
+    if (!time) {
       time = parseFloat(play.duration) * 1000;
     }
     obj.playKeyframe(play);
-    setTimeout(function (){
-      if(typeof(valuesOrCall)=="function"){
+    setTimeout(function () {
+      if (typeof valuesOrCall == "function") {
         return valuesOrCall();
-      }else if(typeof(callOrTime)=="function"){
+      } else if (typeof callOrTime == "function") {
         return callOrTime();
       }
     }, time);
-    
-  }else{
-    if(!time){
-      time = parseFloat(play)*1000;
+  } else {
+    if (!time) {
+      time = parseFloat(play) * 1000;
     }
-    obj.playKeyframe(temp.name+play);
-    setTimeout(function (){
-      if(typeof(valuesOrCall)=="function"){
+    obj.playKeyframe(temp.name + play);
+    setTimeout(function () {
+      if (typeof valuesOrCall == "function") {
         return valuesOrCall();
-      }else if(typeof(callOrTime)=="function"){
+      } else if (typeof callOrTime == "function") {
         return callOrTime();
       }
     }, time);
@@ -502,33 +475,33 @@ valuesOrCall={}, callOrTime, time){
 var shrink = {
   name: "shrink",
   from: {
-    height: "300px"
+    height: "300px",
   },
   to: {
-    height: "0px"
-  }
-}
+    height: "0px",
+  },
+};
 var stretch = {
   name: "stretch",
   "0%": {
-    width: "0px"
+    width: "0px",
   },
   "100%": {
-    width: "600px"
-  }
-}
+    width: "600px",
+  },
+};
 var expand = {
   name: "expand",
   "0%": {
     visibility: "hidden",
     right: "125px",
     width: 0,
-    height: 0
+    height: 0,
   },
   "100%": {
     visibility: "visible",
     right: 0,
     width: "250px",
-    height: "250px"
-  }
-}
+    height: "250px",
+  },
+};
