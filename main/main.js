f094b7b4cf106b9558a2c32175302fcb569d566f
Fagner <fagnerrc12@gmail.com>
2023-06-30 19:42:40 -0300

    refactor

--- main/main.js
+++ main/main.js
@@ -1,15 +1,21 @@
 const squareCount = 5;
-let selectedSquares = [];
+let selectedSquares = [null, null];
 let gradientSelected = null;
 let gradSelectorOpened = false;
 
-(() => {
+function initialize() {
   document.body.style.background = getRandomColor();
   for (let i = 1; i <= squareCount; i++) {
     square = document.querySelector(`#s${i}`);
     square.style.background = getRandomColor();
   }
-})();
+  const borderColor = getRandomColor();
+  document
+    .querySelectorAll(".border-box")
+    .forEach((x) => (x.style.borderColor = borderColor));
+}
+
+initialize();
 
 async function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -31,12 +37,15 @@ async function openGradientSelector() {
 }
 
 async function closeGradientSelector() {
-  if (selectedSquares.length > 0) {
-    while (selectedSquares.length > 0) {
-      toggleSelected(selectedSquares[0]);
+  let squaresDeleted = false;
+  for (var i = 0; i < selectedSquares.length; i++) {
+    if (selectedSquares[i] != null) {
+      toggleSelected(selectedSquares[i]);
+      squaresDeleted = true;
     }
-    await wait(200);
   }
+  if (squaresDeleted) await wait(200);
+
   const selector = document.querySelector(".gradient-selector");
 
   selector.className = "gradient-selector";
@@ -59,12 +68,15 @@ async function openSolidSelector() {
 }
 
 async function closeSolidSelector() {
-  if (selectedSquares.length > 0) {
-    while (selectedSquares.length > 0) {
-      toggleSelected(selectedSquares[0]);
+  let squaresDeleted = false;
+  for (var i = 0; i < selectedSquares.length; i++) {
+    if (selectedSquares[i] != null) {
+      toggleSelected(selectedSquares[i]);
+      squaresDeleted = true;
     }
-    await wait(100);
   }
+  if (squaresDeleted) await wait(100);
+
   const selector = document.querySelector(".solid-selector");
 
   selector.className = "solid-selector";
@@ -82,38 +94,48 @@ function toggleSelected(i) {
     square.style.left = "0px";
     square.style.scale = "1";
     square.style.rotate = "0deg";
-    selectedSquares.splice(selected, 1);
+    selectedSquares[selected] = null;
     return;
   }
-
-  square.style.scale = "2";
 
   if (gradSelectorOpened) {
+    const selected = document.querySelector("#s" + gradientSelected);
+    if (selected) {
+      selected.style.scale = "1";
+      square.classList.remove("select-square");
+    }
     gradientSelected = i;
+    square.classList.add("select-square");
+    square.style.scale = "1.2";
     return;
   }
 
-  if (selectedSquares.length > 0) {
-    toggleSelected(selectedSquares[0]);
+  const next = selectedSquares.find((x) => x != null);
+  if (next) {
+    toggleSelected(next);
   }
 
-  selectedSquares.push(i);
+  selectedSquares[0] = i;
   moveSquare(i, 0);
   setColor(square.style.background);
 }
 
 function setGradient(slotNumber) {
   if (gradientSelected == null) return;
-
   moveSquare(gradientSelected, slotNumber);
-  selectedSquares.push(gradientSelected);
+  slotNumber -= 1;
+
+  if (selectedSquares[slotNumber] != null) {
+    toggleSelected(selectedSquares[slotNumber]);
+  }
+  selectedSquares[slotNumber] = gradientSelected;
   const colors = [];
   for (var i = 0; i < selectedSquares.length; i++) {
     colors.push(
       document.querySelector("#s" + selectedSquares[i])?.style.background
     );
   }
-  setColor(colors[1], colors[0]);
+  setColor(colors[0], colors[1]);
   gradientSelected = null;
 }
 
@@ -123,6 +145,9 @@ function moveSquare(id, slotNumber) {
     [slotNumber].getBoundingClientRect();
 
   const square = document.querySelector(`#s${id}`);
+  square.classList.remove("select-square");
+  square.style.scale = "2";
+
   const containerRect = square.getBoundingClientRect();
   square.style.top = `${
     selectorsRect.top +
@@ -139,43 +164,20 @@ function moveSquare(id, slotNumber) {
 }
 
 function toggleSection() {
-  const section = document.querySelector("#first");
+  const section1 = document.querySelector("#first");
+  const section2 = document.querySelector("#second");
+
   const button = document.querySelector("#animated-dot");
-  if (section.classList.contains("hidden")) {
-    section.classList.remove("hidden");
+  if (section1.classList.contains("hidden")) {
+    section1.classList.remove("hidden");
+    section2.classList.add("hidden");
     button.style.rotate = "45deg";
   } else {
-    section.classList.add("hidden");
+    section1.classList.add("hidden");
+    section2.classList.remove("hidden");
     button.style.rotate = "225deg";
   }
 }
-/*================[CLEAR]==================*/
-// function clear() {
-//   gsq.forEach(function (x, key) {
-//     if (x >= 0) {
-//       ani(x, true);
-//     }
-//     setTimeout(function () {
-//       gsq.set(key, -1);
-//     }, 500);
-//   });
-//   if (selectedSquare) {
-//     ani(selectedSquare, true);
-//     setTimeout(function () {
-//       selectedSquare = null;
-//     }, 500);
-//   }
-
-//   selected = null;
-// }
-
-// function borderColor(color) {
-//   var borders = document.getElementsByClassName("border");
-//   for (var i = 0; i < borders.length; i++) {
-//     borders[i].style.borderColor = color;
-//   }
-//   document.getElementsByClassName("b")[0].style.backgroundColor = color;
-// }
 
 function setColor(c1, c2 = null) {
   if (c2 == null) c2 = c1;
@@ -190,121 +192,3 @@ function getRandomColor() {
   }
   return color;
 }
-
-// var fill1 = $(".fill2");
-// var fill = $(".fill1");
-
-// var sqBack = {
-//   name: "sqBack",
-//   "0%": {
-//     top: "225px",
-//     left: "293px",
-//     transform: "rotate(90deg) scale(1.5,1.5)",
-//   },
-//   "100%": {
-//     top: "0px",
-//     left: "0px",
-//     transform: "rotate(0deg) scale(1, 1)",
-//   },
-// };
-// var sqForward = {
-//   name: "sqForward",
-//   "0%": {
-//     top: "0px",
-//     left: "0px",
-//     transform: "rotate(0deg) scale(1, 1)",
-//   },
-//   "100%": {
-//     top: "225px",
-//     left: "293px",
-//     transform: "rotate(90deg) scale(1.5,1.5)",
-//   },
-// };
-
-// var frameNames = {};
-// function animate(obj, frame, play, valuesOrCall = {}, callOrTime, time) {
-//   var temp = frame;
-//   if (typeof valuesOrCall == "object") {
-//     for (const [key1, value1] of Object.entries(valuesOrCall)) {
-//       for (const [key2, value2] of Object.entries(value1)) {
-//         temp[key1][key2] = value2;
-//       }
-//     }
-//   }
-//   var anNum = 0;
-//   if (temp.name in frameNames) {
-//     anNum = 1;
-//     while (frameNames[temp.name].indexOf(anNum) >= 0) {
-//       anNum++;
-//     }
-//     frameNames[temp.name].push(anNum);
-//   } else {
-//     frameNames[temp.name] = [0];
-//   }
-//   temp.name = temp.name + anNum;
-
-//   $.keyframe.define([temp]);
-//   if (typeof callOrTime == "number") {
-//     time = callOrTime;
-//   }
-//   if (typeof play == "object") {
-//     play["name"] = temp.name;
-//     if (!time) {
-//       time = parseFloat(play.duration) * 1000;
-//     }
-//     obj.playKeyframe(play);
-//     setTimeout(function () {
-//       if (typeof valuesOrCall == "function") {
-//         return valuesOrCall();
-//       } else if (typeof callOrTime == "function") {
-//         return callOrTime();
-//       }
-//     }, time);
-//   } else {
-//     if (!time) {
-//       time = parseFloat(play) * 1000;
-//     }
-//     obj.playKeyframe(temp.name + play);
-//     setTimeout(function () {
-//       if (typeof valuesOrCall == "function") {
-//         return valuesOrCall();
-//       } else if (typeof callOrTime == "function") {
-//         return callOrTime();
-//       }
-//     }, time);
-//   }
-// }
-// var shrink = {
-//   name: "shrink",
-//   from: {
-//     height: "300px",
-//   },
-//   to: {
-//     height: "0px",
-//   },
-// };
-// var stretch = {
-//   name: "stretch",
-//   "0%": {
-//     width: "0px",
-//   },
-//   "100%": {
-//     width: "600px",
-//   },
-// };
-// var expand = {
-//   name: "expand",
-//   "0%": {
-//     visibility: "hidden",
-//     right: "125px",
-//     width: 0,
-//     height: 0,
-//   },
-//   "100%": {
-//     visibility: "visible",
-//     right: 0,
-//     width: "250px",
-//     height: "250px",
-//   },
-// };
-// //
